ea517d3807dac453aa7e03eef328511393915802
Dani Biro <danipro93@gmail.com>
2022-11-04 22:05:35 +0200

    Move exports

--- src/approx/index.ts
+++
@@ -1,7 +0,0 @@
-export { nthroot, sqrt, cbrt } from './roots';
-export { pow, exp } from './exponential';
-export { log, logn, log10, log2 } from './logarithm';
-export { PI, sin, cos, tan } from './trigonometry';
-export { asin, acos, atan } from './inverse_trigonometry';
-export { sinh, cosh, tanh } from './hyperbolic';
-export { asinh, acosh, atanh } from './inverse_hyperbolic';
